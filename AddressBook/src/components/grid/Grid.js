597f199009182ff9effe38ed0e3672c1a371879c
Desteel <alexgowargame@yandex.ru>
2019-02-24 23:16:54 +0400

    add data .json

--- AddressBook/src/components/grid/Grid.js
+++ AddressBook/src/components/grid/Grid.js
@@ -1,8 +1,39 @@
 import Template from "../card/Card";
 
-require('../../data/items.json');
+let jsonUrl = require('../../data/items.json');
 
 class Grid extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			items: []
+		};
+	}
+	
+	componentDidMount() {
+		fetch(jsonUrl)
+			.then(res => {
+				console.log(res);
+				return res.json();
+			})
+			.then(
+				result => {
+					this.setState({
+						isLoaded: true,
+						items: result.items
+					});
+				},
+				error => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			);
+	}
+	
     render() {
         return (
             <div className="grid">
