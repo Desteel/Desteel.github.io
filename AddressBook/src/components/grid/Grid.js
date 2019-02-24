9cd3939d94eff3a24ac58d33c0a13f4afc109502
Desteel <alexgowargame@yandex.ru>
2019-02-24 23:44:54 +0400

    add data .json

--- AddressBook/src/components/grid/Grid.js
+++ AddressBook/src/components/grid/Grid.js
@@ -14,15 +14,14 @@ class Grid extends React.Component {
 	
 	componentDidMount() {
 		fetch(jsonUrl)
-			.then(res => {
-				console.log(res);
-				return res.json();
-			})
+			.then(res => res.json())
 			.then(
 				result => {
 					this.setState({
 						isLoaded: true,
 						items: result.items
+					}, () => {
+						//console.log(this.state)
 					});
 				},
 				error => {
@@ -35,7 +34,40 @@ class Grid extends React.Component {
 	}
 	
     render() {
-        return (
+		const { error, isLoaded, items } = this.state;
+		
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div className="grid">
+					{items.map(item => (
+						<Template
+							data={{
+								key: "1",
+								photoUrl:
+									"https://avatars3.githubusercontent.com/u/30462928?s=460&v=4",
+								name: "Alexander",
+								surname: "Kerensky",
+								phone: "84626358367",
+								address: "Detroit"
+							}}
+						/>
+						
+					))}					
+				</div>
+				/*
+				<li key={item.name}>
+					{item.name} {item.price}
+				</li>
+				*/
+			);
+		}
+	  
+        /*
+		return (
             <div className="grid">
                 <Template
                     data={{
@@ -49,6 +81,7 @@ class Grid extends React.Component {
                 />
             </div>
         );
+		*/
     }
 }
 
