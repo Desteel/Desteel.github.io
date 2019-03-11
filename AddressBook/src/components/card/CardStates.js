834cfdb9a389c5c958fa57c733ffdd00e6145a40
Desteel <alexgowargame@yandex.ru>
2019-03-11 23:53:56 +0400

    add card state form

--- AddressBook/src/components/card/CardStates.js
+++ AddressBook/src/components/card/CardStates.js
@@ -1,35 +1,25 @@
-function Form({action, children}) {
+function Form({ action, children, ...rest }) {
     return (
         <form className="card">
-                <div className="card__imagebox">
-                    <img src={setPhoto} />
-                </div>
-                <div className="card__main">
-                    <ElemInput
-                        templateEdit={action}
-                        value={name}
-                        cath={"name"}
-                    />
-                    <ElemInput
-                        templateEdit={action}
-                        value={surname}
-                        cath={"surname"}
-                    />
-                    <ElemInput
-                        templateEdit={action}
-                        value={phone}
-                        cath={"phone"}
-                    />
-                    <ElemInput
-                        templateEdit={action}
-                        value={address}
-                        cath={"address"}
-                    />
-                </div>
-                {
-                    children
-                }
-            </form>
+            <div className="card__imagebox">
+                <img src={setPhoto} />
+            </div>
+            <div className="card__main">
+                <ElemInput templateEdit={action} value={name} cath={"name"} />
+                <ElemInput
+                    templateEdit={action}
+                    value={surname}
+                    cath={"surname"}
+                />
+                <ElemInput templateEdit={action} value={phone} cath={"phone"} />
+                <ElemInput
+                    templateEdit={action}
+                    value={address}
+                    cath={"address"}
+                />
+            </div>
+            {children}
+        </form>
     );
 }
 
