fdb78998ffdc196a61bf63b019806d695b303755
Dest <alexgowargame@yandex.ru>
2019-03-18 16:15:25 +0400

    add modal style

--- AddressBook/src/components/modal/Modal.js
+++ AddressBook/src/components/modal/Modal.js
@@ -1,19 +1,42 @@
+import styled from "styled-components";
 import { observer, inject } from "mobx-react";
 import Button from "../button/Button";
 import IconCross from "../../icons/cross.svg";
+
+const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: auto;
+    z-index: 999;
+    background-color: rgba(0, 0, 0, 0.8);
+`;
+const Body = styled.div`    
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: 20px auto;
+    background-color: #fff;
+    max-width: 900px;
+`;
 
 @inject("modalStore")
 @observer
 class Modal extends React.Component {
     render() {
-        const { isOpen, close, content } = this.props.modalStore;        
+        const { isOpen, close, content } = this.props.modalStore;
 
         if (isOpen) {
             return (
-                <div>
-                    <div>{content}</div>
-                    <Button action={close}>{<IconCross/>}</Button>
-                </div>
+                <Overlay onClick={close}>
+                    <Body>
+                        <div>{content}</div>
+                        <Button action={close}>{<IconCross />}</Button>
+                    </Body>
+                </Overlay>
             );
         } else {
             return null;
