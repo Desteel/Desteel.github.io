4e721d27fa739362a39a666bd53109adba20589a
Dest <alexgowargame@yandex.ru>
2019-03-19 16:59:05 +0400

    Separation of styles

--- AddressBook/src/App.js
+++ AddressBook/src/App.js
@@ -1,45 +1,22 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { Provider } from "mobx-react";
 import { observer } from "mobx-react";
+// stores
 import searchStore from "./components/stores/SearchStore";
 import contentStore from "./components/stores/ContentStore";
 import modalStore from "./components/stores/ModalStore";
+
 import Header from "./components/main/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/main/footer/Footer";
 import Modal from "./components/modal/Modal";
 import Normalize from "./styles/Normalize";
+import { theme, GlobalStyle } from "./styles/StyledGlobal";
 
 import DevTools from "mobx-react-devtools";
 
 const stores = { searchStore, contentStore, modalStore };
-
-const theme = {
-    main: "#dadce0",
-    border: "1px solid",
-    borderRadius: "8px"
-};
-
-const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&subset=cyrillic');
-    body {
-        font-family: 'Roboto', sans-serif;
-        color: rgba(0,0,0, 0.54);        
-    }
-    .page {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-    }
-    .main {
-        flex: 1 0 auto;
-    }
-    .container {
-        margin: 0 auto;
-        padding: 0 15px;
-        max-width: 1300px;
-    }
-`;
 
 @observer
 class App extends React.Component {
@@ -49,7 +26,7 @@ class App extends React.Component {
                 <ThemeProvider theme={theme}>
                     <React.Fragment>
                         <Normalize />
-                        <GlobalStyle />
+                        <GlobalStyle modalState={modalStore.isOpen} />
                         <DevTools />
                         <Header />
                         <Main />
