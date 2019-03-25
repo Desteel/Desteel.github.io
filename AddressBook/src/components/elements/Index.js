5fd7b962f191b5c27a2a0aa62d36e25c2d661f95
Desteel <alexgowargame@yandex.ru>
2019-03-25 23:16:33 +0400

    add Name for button elements

--- AddressBook/src/components/elements/Index.js
+++ AddressBook/src/components/elements/Index.js
@@ -1,7 +1,7 @@
 import { StyledButton as Btn } from "./Styles";
 
 const Button = ({ action, children, className, ...rest }) => (
-    <Btn type="button" onClick={action} className={className}>
+    <Btn type="button" onClick={action} className={className} name={rest.name}>
         {children}
     </Btn>
 );
