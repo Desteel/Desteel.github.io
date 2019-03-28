e10e9bfa3717131edd3e887ea2fdfea9b97bd40b
Desteel <alexgowargame@yandex.ru>
2019-03-28 21:36:01 +0400

    add set type

--- AddressBook/src/components/elements/Index.js
+++ AddressBook/src/components/elements/Index.js
@@ -1,7 +1,7 @@
 import { StyledButton as Btn } from "./Styles";
 
 const Button = ({ action, children, className, ...rest }) => (
-    <Btn type="button" onClick={action} className={className} name={rest.name}>
+    <Btn type={rest.type ? rest.type : "button" } onClick={action} className={className} name={rest.name}>
         {children}
     </Btn>
 );
