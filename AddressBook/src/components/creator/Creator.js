04caba6e65e2185ae207782d5e7f1999dcf69577
Desteel <alexgowargame@yandex.ru>
2019-03-28 21:36:30 +0400

    init final form in creator

--- AddressBook/src/components/creator/Creator.js
+++ AddressBook/src/components/creator/Creator.js
@@ -4,142 +4,94 @@ import {
     Inputbox,
     Input,
     Row
-} from './StyledCreator';
-import { observable, action } from 'mobx';
-import { observer, inject } from 'mobx-react';
-import IconCheck from '../../icons/tick.svg';
-import { createGuid } from '../../utils';
+} from "./StyledCreator";
+import { action } from "mobx";
+import { observer, inject } from "mobx-react";
+import { Form } from "react-final-form";
+import arrayMutators from "final-form-arrays";
+import { FieldArray } from "react-final-form-arrays";
+import IconCheck from "../../icons/tick.svg";
 
-@inject('contentStore', 'modalStore', 'creatorStore')
+@inject("contentStore", "modalStore", "creatorStore")
 @observer
 class Creator extends React.Component {
-    // @observable template = {
-    //     id: "",
-    //     photoUrl: "",
-    //     name: "",
-    //     surname: "",
-    //     phoneValues: [""],
-    //     address: ""
-    // };
-
-    // @action("add phones input")
-    // addInput = () => {
-    //     this.template = {
-    //         ...this.template,
-    //         phoneValues: [...this.template.phoneValues, ""]
-    //     };
-    // };
-
-    // @action("remove phones input")
-    // removeInput = key => () => {
-    //     this.template = {
-    //         ...this.template,
-    //         phoneValues: this.template.phoneValues.filter((v, i) => i !== key)
-    //     };
-    // };
-
-    // @action("fill card phones")
-    // fillPhones = key => e => {
-    //     const { value: newValue } = e.target;
-
-    //     this.template = {
-    //         ...this.template,
-    //         phoneValues: this.template.phoneValues.map(
-    //             (value, i) => (i === key ? newValue : value)
-    //         )
-    //     };
-    // };
-
-    // @action("fill card string")
-    // fillString = e => {
-    //     const { value, name } = e.target;
-
-    //     this.template = {
-    //         ...this.template,
-    //         [name]: value
-    //     };
-    // };
-
-    // @action("create card")
-    // createCard = () => {
-    //     this.template = {
-    //         ...this.template,
-    //         id: createGuid(),
-    //         phoneValues: this.template.phoneValues.filter(value => !!value)
-    //     };
-    // };
-
-    @action('create and add card ')
-    handleClick = () => {
-        this.props.creatorStore.createCard();        
-        this.props.action(this.props.creatorStore.template);
+    @action("creator submit")
+    onSubmit = values => {
+        console.log(JSON.stringify(values, 0, 2));
     };
 
-    componentWillMount () {
-        this.props.creatorStore.template = {
-            id: "",
-            photoUrl: "",
-            name: "",
-            surname: "",
-            phoneValues: [""],
-            address: ""
-        };
-    }
+    @action("remove input")
+    addInput = () => () => {
+        push("numbers", undefined);
+    };
+
+    @action("remove input")
+    removeInput = (fields, i) => () => {
+        fields.remove(i);
+    };
 
     render() {
-        const {
-                fillString,
-                fillPhones,
-                addInput,
-                removeInput
-            } = this.props.creatorStore,
-            { phoneValues } = this.props.creatorStore.template;
-
-        // console.log(this.props.creatorStore.template);
-
         return (
-            <form>
-                <Inputbox>
-                    <Input
-                        onChange={fillString}
-                        name="name"
-                        placeholder="name"
-                    />
-                    <Input
-                        onChange={fillString}
-                        name="surname"
-                        placeholder="surname"
-                    />
-                    {phoneValues.map((value, i) => (
-                        <Row key={i}>
+            <Form
+                onSubmit={this.onSubmit}
+                mutators={{
+                    ...arrayMutators
+                }}
+                render={({
+                    handleSubmit,
+                    form: {
+                        mutators: { push }
+                    }
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <Inputbox>
                             <Input
-                                onChange={fillPhones(i)}
-                                name="phoneValues"
-                                placeholder="phone"
-                                defaultValue={value}
+                                component='input'
+                                name='name'
+                                placeholder='name'
+                                type='text'
                             />
-                            {i === 0 ? (
-                                <Option action={addInput}>add phone</Option>
-                            ) : (
-                                <Option
-                                    action={removeInput(i)}
-                                    name={'phoneValues'}
-                                >
-                                    remove phone
-                                </Option>
-                            )}
-                        </Row>
-                    ))}
-                    <Input
-                        onChange={fillString}
-                        name="address"
-                        placeholder="address"
-                    />
-                </Inputbox>
-                <Button action={this.handleClick}>
-                    <IconCheck />
-                </Button>
-            </form>
+                            <Input
+                                component='input'
+                                name='lastname'
+                                placeholder='lastname'
+                                type='text'
+                            />
+                            <Option action={() => push("numbers", undefined)}>
+                                Add Customer
+                            </Option>
+                            <FieldArray name='numbers'>
+                                {({ fields }) =>
+                                    fields.map((name, i) => (
+                                        <Row key={name}>
+                                            <Input
+                                                component='input'
+                                                name={name}
+                                                placeholder='phone'
+                                                type='phone'
+                                            />
+                                            <Option
+                                                action={() => fields.remove(i)}
+                                            >
+                                                ❌
+                                            </Option>
+                                        </Row>
+                                    ))
+                                }
+                            </FieldArray>
+                            <Input
+                                component='input'
+                                name='address'
+                                placeholder='address'
+                                type='text'
+                            />
+                        </Inputbox>
+                        <Button type={"submit"}>
+                            <IconCheck />
+                        </Button>
+                    </form>
+                )}
+            />
         );
     }
 }
