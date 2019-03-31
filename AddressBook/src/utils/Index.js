3e6373d542d081e7f33fb5a3c84d74acad34c9b9
Desteel <alexgowargame@yandex.ru>
2019-03-31 22:04:47 +0400

    add isNumeric

--- AddressBook/src/utils/Index.js
+++ AddressBook/src/utils/Index.js
@@ -1,3 +1,5 @@
 const S4 = () => (((1+Math.random())*0x10000)|0).toString(16).substring(1)
 
 export const createGuid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase()
+
+export const isNumeric = (n) => !isNaN(parseFloat(n)) && isFinite(n);
