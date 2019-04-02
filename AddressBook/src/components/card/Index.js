5530dbc5f311648b2969aca912e1daa2f8e5fb57
Dest <alexgowargame@yandex.ru>
2019-04-02 10:10:06 +0400

    refactoring

--- AddressBook/src/components/card/Index.js
+++ AddressBook/src/components/card/Index.js
@@ -1,16 +1,16 @@
 import { observable, action } from 'mobx';
 import { observer, inject } from 'mobx-react';
-import ItemForm from './ItemForm';
+import CardForm from '../../components/form/CardForm';
 import ItemDiv from './ItemDiv';
 import Options from '../options/Options';
 
-@inject('contentStore')
+@inject('contentStore', 'modalStore')
 @observer
 class Card extends React.Component {
     @observable editable = false;
 
-    @action('edit card start')
-    editStart = () => {
+    @action('edit card open')
+    editOpen = () => {
         this.editable = true;
     };
 
@@ -19,20 +19,27 @@ class Card extends React.Component {
         this.editable = false;
     };
 
+    @action('save card')
+    saveCard = data => {
+        this.props.contentStore.saveCard(this.props.item.id, data);
+        this.editable = false;
+        // this.props.modalStore.isOpen = false;
+    };
+
     @action('delete card')
     deleteCard = () => this.props.contentStore.deleteCard(this.props.id);
 
     editRender = () => (
-        <ItemForm item={this.props.item} editClose={this.editClose}>
+        <CardForm onSubmit={this.saveCard} initialValues={this.props.item}>
             <Options editable={this.editable} editClose={this.editClose} />
-        </ItemForm>
+        </CardForm>
     );
 
     viewRender = () => (
         <ItemDiv item={this.props.item}>
             <Options
                 editable={this.editable}
-                editStart={this.editStart}
+                editStart={this.editOpen}
                 deleteCard={this.deleteCard}
             />
         </ItemDiv>
