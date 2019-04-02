30e202beff372a947a878ca0e4fa9cba2d371a4d
Dest <alexgowargame@yandex.ru>
2019-04-02 12:04:32 +0400

    refactoring

--- AddressBook/src/components/time/Time.js
+++ AddressBook/src/components/time/Time.js
@@ -1,32 +1,28 @@
 class Time extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-				date: new Date()
-			};
-	}	
-	
-	updateDate() {
-		this.setState({
-			date: new Date()
-		});		
-	}
-	
-	componentDidMount() {
-		let timer = setInterval(() => this.updateDate(), 1000);
-	}
+    constructor() {
+        super();
+        this.state = {
+            date: new Date()
+        };
+    }
 
-	componentWillUnmount() {		
-		clearInterval(timer);
-	}	
-	
-	render() {		
-		return (
-			<div>
-				{this.state.date.toLocaleTimeString()}
-			</div>
-		)
-	}    
+    updateDate() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    componentDidMount() {
+        let timer = setInterval(() => this.updateDate(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(timer);
+    }
+
+    render() {
+        return <div>{this.state.date.toLocaleTimeString()}</div>;
+    }
 }
 
-export default Time
+export default Time;
