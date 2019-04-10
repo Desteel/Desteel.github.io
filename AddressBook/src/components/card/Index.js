2692b74fbfc22c2d2be377b2c63b38f0ea7a504c
Dest <alexgowargame@yandex.ru>
2019-04-10 10:47:44 +0400

    refactoring

--- AddressBook/src/components/card/Index.js
+++ AddressBook/src/components/card/Index.js
@@ -1,35 +1,32 @@
-import { observable, action } from "mobx";
-import { observer, inject } from "mobx-react";
-import CardForm from "../../components/form/CardForm";
-import ItemDiv from "./ItemDiv";
-import Options from "../options/Options";
+import { observable, action } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import CardForm from '../../components/form/CardForm';
+import ItemDiv from './ItemDiv';
+import Options from '../options/Options';
 
-@inject("contentStore", "modalStore")
+@inject('contentStore', 'modalStore')
 @observer
 class Card extends React.Component {
     @observable editable = false;
 
-    @action("toggle card editable")
+    @action('toggle card editable')
     toggleEditable = () => {
         this.editable = !this.editable;
         console.log(this.editable);
     };
 
-    @action("save card")
+    @action('save card')
     saveCard = data => {
         this.props.contentStore.saveCard(data);
         this.editable = false;
         // this.props.modalStore.isOpen = false;
     };
 
-    @action("delete card")
+    @action('delete card')
     deleteCard = () => this.props.contentStore.deleteCard(this.props.id);
 
     editRender = () => (
-        <CardForm
-            onSubmit={this.saveCard}
-            initialValues={{ ...this.props.item }}
-        >
+        <CardForm onSubmit={this.saveCard} initialValues={this.props.item}>
             <Options editable={this.editable} editClose={this.toggleEditable} />
         </CardForm>
     );
