90412d0898bdea68ae03667253ea67d165a74ecf
Dest <alexgowargame@yandex.ru>
2019-04-19 00:23:33 +0400

    refactoring

--- AddressBook/src/utils/Validate.ts
+++ AddressBook/src/utils/Validate.ts
@@ -1,22 +1,22 @@
 import { isNumeric } from '.';
 
-interface validatorsRest {
-    mustBeNumber(): string;
-    minLength(): string;
-    required(): string;
+interface returnString {
+    (value: string): string;
+    // (min: number): string;
+
+    // (foo: string, bar?: number, ...others: boolean[]): number;
 }
 
-export const required = (value: string) => (value ? undefined : 'Required');
+export const required: (value: string) => string | undefined = value =>
+    value ? undefined : 'Required';
 
-export const mustBeNumber = (value: string) =>
+export const mustBeNumber: (value: string) => string | undefined = value =>
     isNumeric(value) ? undefined : 'Must be a number';
 
 export const minLength = (min: number) => (value: string) =>
     isNumeric(value) && value.length >= min ? undefined : `Min length ${min}`;
 
-export const composeValidators = (...validators: validatorsRest[]) => (
-    value: string
-) =>
+export const composeValidators = (...validators) => (value: string) =>
     validators.reduce(
         (error, validator) => error || validator(value),
         undefined
