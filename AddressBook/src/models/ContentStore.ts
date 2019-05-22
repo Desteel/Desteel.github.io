5ca34a65a71cec2d02dd890b19a726aa96460793
Desteel <30462928+Desteel@users.noreply.github.com>
2019-05-23 02:00:09 +0400

    rename 'items' on 'card'

--- AddressBook/src/models/ContentStore.ts
+++ AddressBook/src/models/ContentStore.ts
@@ -2,7 +2,7 @@ import { types, Instance } from 'mobx-state-tree';
 import { Card } from './Card';
 
 export const ContentStore = types.model('ContentStore', {
-    items: types.array(Card)
+    cards: types.array(Card)
 });
 
 export type TypeContentStore = Instance<typeof ContentStore>;
