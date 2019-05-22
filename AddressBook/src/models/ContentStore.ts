f7f6160fa62f19ae43353d21040ed2349592b9c9
Desteel <30462928+Desteel@users.noreply.github.com>
2019-05-23 03:23:58 +0400

    add option remove item

--- AddressBook/src/models/ContentStore.ts
+++ AddressBook/src/models/ContentStore.ts
@@ -1,8 +1,14 @@
-import { types, Instance } from 'mobx-state-tree';
+import { types, destroy, Instance } from 'mobx-state-tree';
 import { Card } from './Card';
 
-export const ContentStore = types.model('ContentStore', {
-    cards: types.array(Card)
-});
+export const ContentStore = types
+    .model('ContentStore', {
+        cards: types.array(Card)
+    })
+    .actions(self => ({
+        remove(card) {
+            destroy(card);
+        }
+    }));
 
 export type TypeContentStore = Instance<typeof ContentStore>;
--- AddressBook/src/models/card.ts
+++ AddressBook/src/models/card.ts
@@ -1,12 +1,18 @@
-import { types, Instance } from 'mobx-state-tree';
+import { types, getParent, Instance } from 'mobx-state-tree';
 
-export const Card = types.model('Card', {
-    id: types.identifier,
-    photoUrl: types.maybe(types.string),
-    name: types.string,
-    lastname: types.string,
-    phoneValues: types.array(types.string),
-    address: types.string
-});
+export const Card = types
+    .model('Card', {
+        id: types.identifier,
+        photoUrl: types.maybe(types.string),
+        name: types.string,
+        lastname: types.string,
+        phoneValues: types.array(types.string),
+        address: types.string
+    })
+    .actions(self => ({
+        remove() {
+            getParent(self, 2).remove(self);
+        }
+    }));
 
 export type TypeCard = Instance<typeof Card>;
