4788666c6f70e116f0e78d165e68338d177bf2ef
Desteel <30462928+Desteel@users.noreply.github.com>
2019-05-23 01:32:22 +0400

    test new content store MST

--- AddressBook/src/containers/card-hoc/Index.js
+++ AddressBook/src/containers/card-hoc/Index.js
@@ -1,14 +1,37 @@
 import { observer, inject } from 'mobx-react';
 import Card from '../../components/card/';
 
-@inject('contentStore')
+@inject('contentStore', 'ContentStore')
 @observer
 class CardCollection extends React.Component {
     componentDidMount() {
         this.props.contentStore.getData();
     }
 
+    contentStore = this.props.ContentStore.create({
+        items: [
+            {
+                photoUrl:
+                    'https://avatars3.githubusercontent.com/u/30462928?s=460&v=4',
+                name: 'test1',
+                lastname: 'test1',
+                phoneValues: ['84626358367', '84626358367'],
+                address: 'Detroit'
+            },
+            {
+                photoUrl:
+                    'https://avatars3.githubusercontent.com/u/30462928?s=460&v=4',
+                name: 'test2',
+                lastname: 'test2',
+                phoneValues: ['84626358367', '84626358367'],
+                address: 'Moscow'
+            }
+        ]
+    });
+
     render() {
+        console.log(this.contentStore.items[0]);
+
         const { error, isLoaded, items } = this.props.contentStore;
 
         if (error) {
@@ -19,6 +42,16 @@ class CardCollection extends React.Component {
             return (
                 <>
                     {items.map(item => (
+                        <Card
+                            key={item.id}
+                            id={item.id}
+                            item={{
+                                ...item,
+                                phoneValues: item.phoneValues.slice()
+                            }}
+                        />
+                    ))}
+                    {this.contentStore.items.map(item => (
                         <Card
                             key={item.id}
                             id={item.id}
