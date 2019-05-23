bdc52f82049d9462a3698c304de6af4a36c80aff
Desteel <30462928+Desteel@users.noreply.github.com>
2019-05-24 02:48:14 +0400

    remove MST create store

--- AddressBook/src/App.tsx
+++ AddressBook/src/App.tsx
@@ -2,12 +2,13 @@
 import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Provider, observer } from 'mobx-react';
-import { Observer } from 'mobx-react'
 // stores
 import searchStore from './stores/SearchStore';
+// test
+import contentStoreMST from './stores/ContentStoreMST';
+// test
 import contentStore from './stores/ContentStore';
 import modalStore from './stores/ModalStore';
-import { ContentStore } from './models/ContentStore';
 
 import Header from './components/main/header/Header';
 import Main from './components/main/main/Main';
@@ -18,45 +19,8 @@ import { theme, GlobalStyle } from './styles/StyledGlobal';
 
 import DevTools from 'mobx-react-devtools';
 
-// store
-// const StoreContext = React.createContext(store);
-
-// interface Props {
-//     children(store: Store): JSX.Element | null;
-// }
-
-// export const WithStore: React.FunctionComponent<Props> = ({ children }) => (
-//     <StoreContext.Consumer>
-//         {store => <Observer>{() => children(store)}</Observer>}
-//     </StoreContext.Consumer>
-// );
-// store
-
-const contentStoreMST = ContentStore.create({
-    cards: [
-        {
-            id: 'a',
-            photoUrl:
-                'https://avatars3.githubusercontent.com/u/30462928?s=460&v=4',
-            name: 'test1',
-            lastname: 'test1',
-            phoneValues: ['84626358367', '84626358367'],
-            address: 'Detroit'
-        },
-        {
-            id: 'b',
-            photoUrl:
-                'https://avatars3.githubusercontent.com/u/30462928?s=460&v=4',
-            name: 'test2',
-            lastname: 'test2',
-            phoneValues: ['84626358367', '84626358367'],
-            address: 'Moscow'
-        }
-    ]
-});
 const stores = {
     searchStore,
-    ContentStore,
     contentStoreMST,
     contentStore,
     modalStore
