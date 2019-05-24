925e59e22b17983a33101584c07f51ae3f249fa1
Desteel <30462928+Desteel@users.noreply.github.com>
2019-05-24 22:01:01 +0400

    apply prettier

--- AddressBook/src/utils/Index.ts
+++ AddressBook/src/utils/Index.ts
@@ -1,5 +1,20 @@
-const S4 = () => (((1+Math.random())*0x10000)|0).toString(16).substring(1)
+const S4 = () =>
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 
-export const createGuid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase()
+export const createGuid = () =>
+    (
+        S4() +
+        S4() +
+        '-' +
+        S4() +
+        '-4' +
+        S4().substr(0, 3) +
+        '-' +
+        S4() +
+        '-' +
+        S4() +
+        S4() +
+        S4()
+    ).toLowerCase();
 
-export const isNumeric = (n) => !isNaN(parseFloat(n)) && isFinite(n);
+export const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
